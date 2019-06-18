9756b8c12064ba4e146dba0ec5e9d05a4d591dd6
Talal <talal__l@hotmail.com>
2019-06-18 14:18:54 +0300

    Move nodes by dragging them on canvas
    This causes a minor problem with the select operation. Slight movements when
    selecting a node causes it to move.

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ import {
     selectNode,
     deSelectNode,
     clearGraphArea,
-    drawGraph
+    drawGraph,
+    htmlEdgeId,
+    htmlNodeId
 } from "./svgRenderer.js";
 
 let activeGraph = new Graph();
@@ -49,16 +51,80 @@ fileSelector.addEventListener("change", function handleFiles(event) {
     reader.readAsText(files[0]);
 });
 
+// util
+function getCanvasXy(event) {
+    // any object with x,y properties
+    let rect = svgCanvas.getBoundingClientRect();
+    let x = Math.floor(event.clientX - rect.left);
+    let y = Math.floor(event.clientY - rect.top);
+    return [x, y];
+}
 
 // events
+
 svgCanvas.addEventListener("mousedown", event => {
     switch (event.target.tagName) {
     case "circle":
-        clickedEl = {
-            x: parseInt(event.target.getAttribute("cx")),
-            y: parseInt(event.target.getAttribute("cy"))
-        };
-        let clickedNode = activeGraph.findNode(clickedEl);
+        // prepare node to be moved
+
+        // get clicked node by id
+        let clickedNodeEl = event.target;
+        let nodeId = parseInt(
+            clickedNodeEl.getAttribute("id").split("n")[1]
+        );
+        let clickedNode = activeGraph.nodes[nodeId];
+
+        // get connected edges
+        let adjNodes = activeGraph.getAdjList(clickedNode);
+        let edgeElList = adjNodes.map(n2Id => {
+            let n2 = activeGraph.nodes[n2Id];
+            let edgeEl = document.querySelector(
+                `#${htmlEdgeId([clickedNode, n2])}`
+            );
+            if (!edgeEl) {
+                edgeEl = document.querySelector(
+                    `#${htmlEdgeId([n2, clickedNode])}`
+                );
+            }
+            return edgeEl;
+        });
+            // register move event for the node
+        function onMove(event) {
+            // get xy inside canvas
+            let [x, y] = getCanvasXy(event);
+
+            // update svg elements
+            clickedNodeEl.setAttribute("cx", x);
+            clickedNodeEl.setAttribute("cy", y);
+
+            // update each edge first node coordinate
+            edgeElList.forEach(el => {
+                // HACK: check if an undirected edge was drawn in reverse (n2 to n1) in html
+                let coordinate = 1;
+                let first = parseInt(
+                    el
+                        .getAttribute("id")
+                        .split("-")[0]
+                        .split("n")[1]
+                );
+                if (clickedNode.id !== first) {
+                    coordinate = 2;
+                }
+
+                el.setAttribute(`x${coordinate}`, x);
+                el.setAttribute(`y${coordinate}`, y);
+            });
+
+            // update node
+            clickedNode.x = x;
+            clickedNode.y = y;
+        }
+        svgCanvas.addEventListener("mousemove",onMove);
+
+        clickedNodeEl.addEventListener("mouseup", () =>
+            svgCanvas.removeEventListener("mousemove", onMove)
+        );
+
         if (lastSelection === null) {
             selectNode(clickedNode);
             lastSelection = clickedNode;
@@ -87,9 +153,7 @@ svgCanvas.addEventListener("mousedown", event => {
         break;
     case "svg":
         // translate ot svg viewport coordinate
-        let rect = event.target.getBoundingClientRect();
-        let x = Math.floor(event.clientX - rect.left);
-        let y = Math.floor(event.clientY - rect.top);
+        let [x, y] = getCanvasXy(event);
 
         if (lastSelection === null) {
             activeGraph.addNode(new Node(x, y));
@@ -119,8 +183,6 @@ window.addEventListener("keypress", event => {
 
 // export to JSON file
 // import graph from file
-
-
 
 // test
 {
