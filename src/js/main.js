0d834c4d762dde3d78a3f3743b5e9b0c2ebf612b
Talal <talal__l@hotmail.com>
2019-07-04 11:24:49 +0300

    Refactor code and remove unused files

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,155 @@
-import { Graph, Node } from "./graph.js";
-import { SvgRenderer } from "./svgRenderer.js";
+// Graph module extensions
+// Get an object of all adjacent nodes to the given node
+sigma.classes.graph.addMethod("allNeighbors", function(node) {
+    return this.allNeighborsIndex[node.id];
+});
 
-let svgCanvas = document.querySelector("svg");
-svgCanvas.setAttribute("width", `${window.innerWidth * 0.8}`);
-svgCanvas.setAttribute("height", `${window.innerHeight * 0.8}`);
+// Create the main sigma instance
+var sig = new sigma({
+    renderer: {
+        type: "webgl",
+        container: "container"
+    },
+    settings: {
+        doubleClickEnabled: false,
+        autoRescale: false
+    }
+});
 
-let svgRenderer = new SvgRenderer(svgCanvas);
-let activeGraph = new Graph();
-let lastSelection = null;
-let clickedEdge = null;
-let clickedEl = null;
+let cam = sig.camera;
+let container = document.querySelector("#container");
 
-// tool bar
+// Generate a random graph:
+let N = 10;
+let r = Math.min(container.offsetWidth, container.offsetHeight);
+
+for (let i = 0; i < N; i++) {
+    let x = 0;
+    let y = 0;
+
+    let p1 = cam.cameraPosition(x, y);
+    let node = {
+        id: "n" + i,
+        label: "Node " + i,
+        x: p1.x,
+        y: p1.y,
+        size: 10,
+        color: "#921"
+    };
+    sig.graph.addNode(node);
+
+    for (let n of sig.graph.nodes()) {
+        if (node.id === n.id) continue;
+        sig.graph.addEdge({
+            id: `e${node.id}${n.id}`,
+            source: node.id,
+            target: n.id,
+            size: 3,
+            color: "#ccc"
+        });
+    }
+}
+
+// Create graph layout
+let customLayout = new sigma.CustomLayout(sig);
+console.log(customLayout);
+function run() {
+    customLayout.run();
+    sig.refresh();
+}
+function runStep() {
+    customLayout.step();
+    sig.refresh();
+}
+
+// Graph events
+
+
+
+// Add drag support
+let dragListener = sigma.plugins.dragNodes(sig, sig.renderers[0]);
+
+// Track selected node
+let selectedNode = null;
+
+function toggleNode(node) {
+    if (selectedNode === node) {
+        selectedNode.color = "#921";
+        selectedNode = null;
+    } else {
+        node.color = "#c52";
+        selectedNode = node;
+    }
+    sig.refresh();
+}
+
+sig.bind("clickNode", e => {
+    let node = e.data.node;
+    if (selectedNode && selectedNode.id !== node.id) {
+        // Create an edge if non existed between them
+        if (!sig.graph.allNeighbors(node)[selectedNode.id]) {
+            sig.graph.addEdge({
+                id: `e${selectedNode.id}${node.id}`,
+                source: selectedNode.id,
+                target: node.id,
+                size: 3,
+                color: "#ccc"
+            });
+        }
+    }
+    // Toggle node if no selection exist
+    toggleNode(selectedNode || node);
+});
+
+// Reset selection
+sig.bind("clickStage rightClickStage", e => {
+    if (selectedNode) toggleNode(selectedNode);
+});
+
+let clickCount = 0;
+// Disable right click in the graph area
+container.addEventListener("contextmenu", event => event.preventDefault());
+sig.bind("rightClickStage", e => {
+    // The camera starts at the center of the canvas
+    // It is treated as the origin and the coordinates are added to it when the nodes are rendered (when autoRescale is false)
+    // To get the desired position we subtract away the initial coordinate of the camera
+    let x = event.offsetX - container.offsetWidth / 2;
+    let y = event.offsetY - container.offsetHeight / 2;
+
+    // Get x,y after applying the same transformation that were applied to the camera
+    let p = cam.cameraPosition(x, y);
+
+    let n = {
+        label: "click" + clickCount++,
+        id: "click" + clickCount++,
+        x: p.x,
+        y: p.y,
+        size: 10,
+        color: "#021"
+    };
+
+    sig.graph.addNode(n);
+    sig.refresh();
+});
+
+// Tool bar
 const saveGraph = document.querySelector("#saveGraph"),
     fileSelector = document.querySelector("#fileSelector"),
     loadGraph = document.querySelector("#loadGraph"),
     clearGraph = document.querySelector("#clearGraph");
 
 clearGraph.addEventListener("click", () => {
-    activeGraph = new Graph();
-    svgRenderer.clearGraphArea();
+    sig.graph.clear();
+    sig.refresh();
 });
 saveGraph.addEventListener("click", event => {
     let saveGraphLink = document.querySelector("#saveGraphLink");
     let d = new Date();
     let date = `${d.getFullYear()}${d.getDate()}${d.getDate()}${d.getHours()}${d.getMinutes()}${d.getSeconds()}`;
-    let json = JSON.stringify(activeGraph);
+    let json = JSON.stringify({
+        nodes: sig.graph.nodes(),
+        edges: sig.graph.edges()
+    });
     let blob = new Blob([json], { type: "text/plain" });
     let url = window.URL.createObjectURL(blob);
     saveGraphLink.setAttribute("download", date);
@@ -43,215 +167,14 @@ fileSelector.addEventListener("change", function handleFiles(event) {
 
     reader.onload = e => {
         let content = e.target.result;
-        activeGraph = new Graph();
-        Object.assign(activeGraph, JSON.parse(content));
+        console.log(content);
+        sig.graph.clear();
+        sig.graph.read(JSON.parse(content));
         fileSelector.value = "";
-        svgRenderer.drawGraph(activeGraph);
+        sig.refresh();
     };
 
     reader.readAsText(files[0]);
 });
 
-// util
-function getCanvasXy(event) {
-    // any object with x,y properties
-    let rect = svgCanvas.getBoundingClientRect();
-    let x = Math.floor(event.clientX - rect.left);
-    let y = Math.floor(event.clientY - rect.top);
-    return [x, y];
-}
-
-// events
-svgCanvas.addEventListener("mousedown", event => {
-    let isDragged = false;
-
-    switch (event.target.tagName) {
-    case "circle":
-        // prepare node to be moved
-
-        // get clicked node by id
-        let clickedNodeEl = event.target;
-        let nodeId = parseInt(
-            clickedNodeEl.getAttribute("id").split("n")[1]
-        );
-        let clickedNode = activeGraph.nodes[nodeId];
-
-        // record initial x,y pos
-        let delta = clickedNode.r * 2; // how many pixels to move before triggering a move operation
-        let initX = event.x;
-        let initY = event.y;
-
-        // get connected edges
-        let adjNodes = activeGraph.getAdjList(clickedNode);
-        let edgeElList = adjNodes.map(n2Id => {
-            let n2 = activeGraph.nodes[n2Id];
-            let edgeEl = document.querySelector(
-                `#${svgRenderer.htmlEdgeId([clickedNode, n2])}`
-            );
-            if (!edgeEl) {
-                edgeEl = document.querySelector(
-                    `#${svgRenderer.htmlEdgeId([n2, clickedNode])}`
-                );
-            }
-            return edgeEl;
-        });
-            // register move event for the node
-        function onMove(event) {
-            // check if we moved enough to trigger a move operation
-            let deltaX = Math.abs(event.x - initX);
-            let deltaY = Math.abs(event.y - initY);
-            if (deltaX > delta || deltaY > delta) {
-                isDragged = true;
-                // get xy inside canvas
-                initX = initY = 0;
-                let [x, y] = getCanvasXy(event);
-
-                if (lastSelection === null){
-
-                    lastSelection = clickedNode;
-                    svgRenderer.selectNode(lastSelection);
-                }
-                // update svg elements
-                clickedNodeEl.setAttribute("cx", x);
-                clickedNodeEl.setAttribute("cy", y);
-
-                // update each edge first node coordinate
-                edgeElList.forEach(el => {
-                    // HACK: check if an undirected edge was drawn in reverse (n2 to n1) in html
-                    let coordinate = 1;
-                    let first = parseInt(
-                        el
-                            .getAttribute("id")
-                            .split("-")[0]
-                            .split("n")[1]
-                    );
-                    if (clickedNode.id !== first) {
-                        coordinate = 2;
-                    }
-
-                    el.setAttribute(`x${coordinate}`, x);
-                    el.setAttribute(`y${coordinate}`, y);
-                });
-
-                // update node
-                clickedNode.x = x;
-                clickedNode.y = y;
-            }
-        }
-        svgCanvas.addEventListener("mousemove", onMove);
-        svgCanvas.addEventListener("mouseleave", () => {
-            if (isDragged && lastSelection) {
-                svgRenderer.deSelectNode(lastSelection);
-                lastSelection = null;
-                isDragged = false;
-            }
-            svgCanvas.removeEventListener("mousemove", onMove);
-        });
-
-        clickedNodeEl.addEventListener("mouseup", () => {
-            // remove selection if it was a result of a drag
-            if (isDragged) {
-                lastSelection = null;
-                svgRenderer.deSelectNode(clickedNode);
-                isDragged = false;
-            }
-            svgCanvas.removeEventListener("mousemove", onMove);
-        });
-
-        if (lastSelection === null) {
-            svgRenderer.selectNode(clickedNode);
-            lastSelection = clickedNode;
-        } else if (clickedNode === lastSelection) {
-            svgRenderer.deSelectNode(clickedNode);
-            lastSelection = null;
-        } else {
-            let edge = [lastSelection, clickedNode];
-            if (!activeGraph.edgeExist(edge)) {
-                activeGraph.addEdge(edge);
-                svgRenderer.drawEdge(edge);
-            }
-            svgRenderer.deSelectNode(lastSelection);
-            lastSelection = null;
-            svgCanvas.removeEventListener("mousemove", onMove);
-        }
-
-        break;
-    case "line":
-        let p1 = {
-            x: parseInt(event.target.getAttribute("x1")),
-            y: parseInt(event.target.getAttribute("y1"))
-        };
-        let p2 = {
-            x: parseInt(event.target.getAttribute("x2")),
-            y: parseInt(event.target.getAttribute("y2"))
-        };
-        clickedEdge = activeGraph.findEdge(p1, p2);
-        activeGraph.deleteEdge(clickedEdge);
-        svgRenderer.clearEdge(clickedEdge);
-
-        break;
-    case "svg":
-        // translate ot svg viewport coordinate
-        let [x, y] = getCanvasXy(event);
-
-        if (lastSelection === null) {
-            let n = new Node(x, y);
-            activeGraph.addNode(n);
-            svgRenderer.drawNode(n);
-        } else {
-            svgRenderer.deSelectNode(lastSelection);
-        }
-
-        lastSelection = null;
-
-        break;
-    }
-
-    console.log("activeGraph", activeGraph);
-    console.log("lastSelection", lastSelection);
-});
-
-window.addEventListener("keypress", event => {
-    if (event.key === "d" || event.key === "Delete") {
-        if (lastSelection !== null) {
-            activeGraph.deleteNode(lastSelection);
-            svgRenderer.clearNode(lastSelection);
-            console.log("after node delete", activeGraph);
-            lastSelection = null;
-            svgRenderer.drawGraph(activeGraph);
-        }
-        console.log(event);
-    }
-});
-
-// export to JSON file
-// import graph from file
-
-// test
-{
-    console.log("testing");
-    let n = [
-        new Node(281, 378),
-        new Node(650, 287),
-        new Node(421, 230),
-        new Node(588, 455)
-    ];
-
-    let g = new Graph();
-    g.addNode(n[0]);
-    g.addNode(n[1]);
-    g.addNode(n[2]);
-    g.addNode(n[3]);
-
-    g.addEdge([n[0], n[1]]);
-    g.addEdge([n[0], n[3]]);
-    g.addEdge([n[2], n[3]]);
-    g.addEdge([n[1], n[3]]);
-
-    activeGraph = g;
-    svgRenderer.drawGraph(activeGraph);
-
-    console.log("active graph");
-    console.log(activeGraph);
-    // attach graph
-}
+sig.refresh();
--- src/js/util.js
+++
@@ -1,8 +0,0 @@
-export const PI = Math.PI;
-
-export function distance(p1, p2) {
-    let dx = p2.x - p1.x;
-    let dy = p2.y - p1.y;
-    return Math.hypot(dy, dx);
-}
-
