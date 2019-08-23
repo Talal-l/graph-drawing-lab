f8d96586556c135c64364c0657c79566bafdb3ec
Talal <talal__l@hotmail.com>
2019-08-23 22:12:15 +0300

    Validate input in graph generator modal

--- src/js/main.js
+++ src/js/main.js
@@ -51,7 +51,7 @@ console.log(customLayout);
 // util
 
 /**
- * 
+ *
  * @param {object} p1 First point object with x and y as properties
  * @param {object} p2 Second point object with x and y as properties
  * @return {number} distance between the two points
@@ -62,14 +62,14 @@ function distance(p1, p2) {
 /**
  * Generate a random integer between min and max inclusive
  * @param {number} min
- * @param {number} max 
+ * @param {number} max
  * @return {number}
  */
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 /**
- * 
+ *
  * @param {array} array
  */
 function shuffle(array) {
@@ -321,18 +321,61 @@ const genModal = document.querySelector("#gen-modal"),
     genMode = document.querySelector("#gen-mode"),
     nodeNumMinEl = document.querySelector("#node-num-min"),
     nodeNumMaxEl = document.querySelector("#node-num-max"),
+    nodeError = document.querySelector("#node-error"),
     edgeNumMinEl = document.querySelector("#edge-num-min"),
-    edgeNumMaxEl = document.querySelector("#edge-num-max");
-
+    edgeNumMaxEl = document.querySelector("#edge-num-max"),
+    edgeError = document.querySelector("#edge-error");
 genModal.addEventListener("click", event => {
     const target = event.target;
 
     switch (target.id) {
         case "generate":
-            const nodeNumMin = parseInt(nodeNumMinEl.value);
-            const edgeNumMin = parseInt(edgeNumMinEl.value);
-            const nodeNumMax = parseInt(nodeNumMaxEl.value) || nodeNumMin;
-            const edgeNumMax = parseInt(edgeNumMaxEl.value) || edgeNumMin;
+            let maxEdges = null;
+            let nodeNumMin = parseInt(nodeNumMinEl.value),
+                edgeNumMin = parseInt(edgeNumMinEl.value),
+                nodeNumMax = parseInt(nodeNumMaxEl.value),
+                edgeNumMax = parseInt(edgeNumMaxEl.value);
+
+            // toggle any existing error messages
+            nodeError.innerHTML = "";
+            nodeError.style.display = "none";
+            edgeError.innerHTML = "";
+            edgeError.style.display = "none";
+
+            if (genMode.value === "range") {
+                maxEdges = (nodeNumMax * (nodeNumMax - 1)) / 2;
+                console.log(nodeNumMax);
+                if (nodeNumMax < nodeNumMin || !nodeNumMin || !nodeNumMax) {
+                    nodeError.innerHTML = "Max is less than min!";
+                    nodeError.style.display = "block";
+                    break;
+                }
+                if (edgeNumMax < edgeNumMin || !edgeNumMin || !edgeNumMax) {
+                    edgeError.innerHTML = "Max is less than min!";
+                    edgeError.style.display = "block";
+                    break;
+                }
+            } else {
+                maxEdges = (nodeNumMin * (nodeNumMin - 1)) / 2;
+                nodeNumMax = nodeNumMin;
+                edgeNumMax = edgeNumMin;
+            }
+
+            if (nodeNumMin < 1 || !nodeNumMin) {
+                nodeError.innerHTML = "Can't have less than 1 nodes";
+                nodeError.style.display = "block";
+                break;
+            }
+            if (edgeNumMin < nodeNumMin - 1 || !edgeNumMin) {
+                edgeError.innerHTML = `Can't have less than ${nodeNumMin - 2} edges `;
+                edgeError.style.display = "block";
+                break;
+            }
+            if (edgeNumMax > maxEdges || !edgeNumMin) {
+                edgeError.innerHTML = `Can't have more than ${maxEdges} edges`;
+                edgeError.style.display = "block";
+                break;
+            }
             let G = generateGraph(
                 nodeNumMin,
                 nodeNumMax,
@@ -387,6 +430,12 @@ fileSelector.addEventListener("change", function handleFiles(event) {
     reader.readAsText(files[0]);
 });
 genMode.addEventListener("change", event => {
+    // toggle any existing error messages
+    nodeError.innerHTML = "";
+    nodeError.style.display = "none";
+    edgeError.innerHTML = "";
+    edgeError.style.display = "none";
+
     if (event.target.value === "range") {
         nodeNumMaxEl.style.display = "inline";
         edgeNumMaxEl.style.display = "inline";
@@ -493,7 +542,7 @@ function generateGraph(nMin, nMax, eMin, eMax) {
                             console.log(
                                 "Final number of edges: " + G.edges().length
                             );
-                             break;
+                            break;
                         }
                     }
                 }
