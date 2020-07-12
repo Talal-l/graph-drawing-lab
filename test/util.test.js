369cedbe6f7af43a71176738f852bd926586c824
Talal <talal__l@hotmail.com>
2020-07-12 21:08:45 +0300

    Add function for converting from a custom graph format

--- test/util.test.js
+++ test/util.test.js
@@ -18,30 +18,56 @@ import {
 } from "../src/js/util.js";
 import { loadGraph } from "./testUtil.js";
 const Graph = require("graphology");
+const fs = require("fs");
+import { convert } from "./testUtil.js";
 
-test("distance ", () => {
-    let p1 = { x: 0, y: 0 };
-    let p2 = { x: 1, y: 1 };
-    expect(distance(p1, p2)).toBeCloseTo(Math.sqrt(2));
-});
-test("dfs", () => {
-    let order = [];
-    let g = new Graph();
-    g.import({
-        nodes: [{ key: 1 }, { key: 2 }, { key: 3 }, { key: 4 }],
-        edges: [
-            { source: 1, target: 2 },
-            { source: 1, target: 3 },
-            { source: 2, target: 3 },
-            { source: 2, target: 4 }
-        ]
+describe("Misc", () => {
+    test("distance ", () => {
+        let p1 = { x: 0, y: 0 };
+        let p2 = { x: 1, y: 1 };
+        expect(distance(p1, p2)).toBeCloseTo(Math.sqrt(2));
     });
-    dfs(g, "1", nId => {
-        order.push(nId);
+    test("dfs", () => {
+        let order = [];
+        let g = new Graph();
+        g.import({
+            nodes: [{ key: 1 }, { key: 2 }, { key: 3 }, { key: 4 }],
+            edges: [
+                { source: 1, target: 2 },
+                { source: 1, target: 3 },
+                { source: 2, target: 3 },
+                { source: 2, target: 4 }
+            ]
+        });
+        dfs(g, "1", nId => {
+            order.push(nId);
+        });
+        expect(order).toEqual(["1", "2", "3", "4"]);
     });
-    expect(order).toEqual(["1", "2", "3", "4"]);
-});
 
+    test("Should sort edges as expected", () => {
+        // eslint-disable-next-line no-undef
+        let graph = loadGraph(`${__dirname}/data/edgeSortTest.json`);
+        let center = "0";
+        let endPoints = graph.neighbors(center);
+        let baseEndpoint = "1";
+        let sortedEdges = sortNeighborsByAngle(graph, center, baseEndpoint);
+
+        expect(sortedEdges).toEqual(["1", "3", "4", "2"]);
+    });
+
+    test("convert custom format", () => {
+        const file = `${__dirname}/data/customFormat2Cases.txt`;
+        // get all files in directory
+        let fileData = fs.readFileSync(file, "utf-8");
+        let r = convert(fileData, file);
+        let convertedFileGraph = fs.readFileSync(
+            `${__dirname}/data/customFormat2CasesConverted.json`,
+            "utf-8"
+        );
+        expect(JSON.stringify(r[0].data)).toEqual(convertedFileGraph);
+    });
+});
 describe("Vector class", () => {
     test(`The angle should be 0 if one of the vectors is (0,0)`, () => {
         let v1 = new Vec({ x: 0, y: 0 });
@@ -56,18 +82,6 @@ describe("Vector class", () => {
         expect(a).toBeCloseTo(0);
     });
 });
-
-test("Should sort edges as expected", () => {
-    // eslint-disable-next-line no-undef
-    let graph = loadGraph(`${__dirname}/data/edgeSortTest.json`);
-    let center = "0";
-    let endPoints = graph.neighbors(center);
-    let baseEndpoint = "1";
-    let sortedEdges = sortNeighborsByAngle(graph, center, baseEndpoint);
-
-    expect(sortedEdges).toEqual(["1", "3", "4", "2"]);
-});
-
 describe("deepCopy", () => {
     test("simple object", () => {
         let source = { a: 1, b: 2 };
